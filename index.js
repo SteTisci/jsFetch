50348b689c783ed1a0faf907a93f345a28f37c0b
SteTisci <ste.tisci@gmail.com>
2024-05-16 17:01:52 +0200

    rimossi commenti

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ import { URLS } from './urls.js';
 
 
 fetchALL(URLS).then(responses => console.log(responses));
-// #TODO: manipolare risposte HTML per scraper informazioni 
-//      - Regex per estrazione info generica
-//      - modifica fetchURL per formato HTML
-//      - Open data x statistica 
