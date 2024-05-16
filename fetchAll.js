8ef7e438ab91670bbee4bf9ef03f8b039677b574
SteTisci <ste.tisci@gmail.com>
2024-05-16 16:59:57 +0200

    Modifiche generali

--- fetchAll.js
+++ fetchAll.js
@@ -1,20 +1,17 @@
 const fetchURL = async (URL) => {
     const risposta = await fetch(URL);
     return {
-        // Cambiare valori di ritorno con valori open data
-        rispStatus: {
-            stato: risposta.statusText, 
-            codice: risposta.status,
-        }
+        stato: risposta.statusText,
+        codice: risposta.status,
     };
 }
 
 export const fetchALL = async (URLS) => {
-    const risposte = [];
+    const risposte = {};
     // Inoltro richieste http
     for (const URL of URLS) {
         const risposta = await fetchURL(URL);
-        console.log(`Richiesta... ${URL}\t\t...${risposta.rispStatus['stato']}`);
+        console.log(`Richiesta... ${URL}\t\t...${risposta.stato}`);
         risposte[URL] = risposta;
     }
     return risposte;
--- index.js
+++ index.js
@@ -2,12 +2,7 @@ import { fetchALL } from './fetchAll.js';
 import { URLS } from './urls.js';
 
 
-fetchALL(URLS)
-    .then((risposte) => {
-        const statusRisposte = risposte;
-        console.table(statusRisposte);
-        process.exit();
-    });
+fetchALL(URLS).then(responses => console.log(responses));
 // #TODO: manipolare risposte HTML per scraper informazioni 
 //      - Regex per estrazione info generica
 //      - modifica fetchURL per formato HTML
